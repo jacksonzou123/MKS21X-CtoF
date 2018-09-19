980a6f2cf83b35534f8193ce614b83931c223a06
Jackson Zou <jzou00@stuy.edu>
2018-09-18 20:02:45 -0400

    Created temperature change functions

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,5 +1,11 @@
 public class CtoFTester{
   public static void main(String[] args) {
-    System.out.println("Hello world");
+    System.out.println("Hello World");
+  }
+  public static double celsiusToFahrenheit(double c) {
+    return c * (9/5) + 32;
+  }
+  public static double fahrenheitToCelsius(double f) {
+    return (f - 32) * 5/9;
   }
 }
