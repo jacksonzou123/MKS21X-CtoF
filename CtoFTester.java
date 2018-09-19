1fd1afe869f1c42ba61e4aae29f324b2902c782f
Jackson Zou <jzou00@stuy.edu>
2018-09-18 20:10:10 -0400

    Fixed bugs in functions and successfully tested

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,11 +1,12 @@
 public class CtoFTester{
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    System.out.println(celsiusToFahrenheit(100) + " degrees fahrenheit");
+    System.out.println(fahrenheitToCelsius(212) + " degrees celsius");
   }
   public static double celsiusToFahrenheit(double c) {
-    return c * (9/5) + 32;
+    return c * (9.0/5) + 32;
   }
   public static double fahrenheitToCelsius(double f) {
-    return (f - 32) * 5/9;
+    return (f - 32.0) * 5/9;
   }
 }
